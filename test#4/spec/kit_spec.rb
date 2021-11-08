8189c6c8e8a642c290035ee78f65aa20d386599e
anna bozio <anna.bozio@gmail.com>
2021-11-08 18:02:44 +0100

    tests unitaires done

---
+++ test#4/spec/kit_spec.rb
@@ -0,0 +1,22 @@
+require 'kit'
+
+RSpec.describe Kit, '#position' do
+  it "should return an integer" do
+    kit = Kit.new
+    expect(kit.position).to be_a Integer
+  end
+end
+
+RSpec.describe Kit, '#draggable?' do
+  it "should return a boolean" do
+    kit = Kit.new
+    expect(kit.draggable?).to eq(true || false)
+  end
+end
+
+RSpec.describe Kit, '#owners' do
+  it "should return an array" do
+    kit = Kit.new
+    expect(kit.owners).to be_a Array
+  end
+end
