a5dd469034c82a5c0cd1c8bfad3defc8050f59f2
anna bozio <anna.bozio@gmail.com>
2021-11-08 11:20:28 +0100

    btn next previous works

--- test#3/src/controllers/quiz_controller.js
+++ test#3/src/controllers/quiz_controller.js
@@ -1,4 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["page1", "page2", "page3", "results", "answer"]
+
+  goTo2() {
+    this.page1Target.classList.add("d-none")
+    this.page2Target.classList.remove("d-none")
+  }
+
+  goTo3() {
+    this.page2Target.classList.add("d-none")
+    this.page3Target.classList.remove("d-none")
+  }
+
+  goBackTo1() {
+    this.page1Target.classList.remove("d-none")
+    this.page2Target.classList.add("d-none")
+  }
+  goBackTo2() {
+    this.page2Target.classList.remove("d-none")
+    this.page3Target.classList.add("d-none")
+  }
+  goToResults() {
+    this.page3Target.classList.add("d-none")
+    this.resultsTarget.classList.remove("d-none")
+  }
+
+  select() {
+    this.answerTargets.forEach((answer) => {
+      console.log(answer)
+      answer.classList.toggle("selected")
+    });
+
+  }
 }
